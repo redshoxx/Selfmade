bbca216eb12ef989d5406915d06b43222d6ed378
redshoxx <48377562+redshoxx@users.noreply.github.com>
2026-07-31 09:55:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,13 @@ export default defineConfig(({ mode }) => ({
   define:
     mode === 'demo'
       ? {
-          'import.meta.env.VITE_SUPABASE_URL': '"https://ecflcrigkfyhifekwfxq.supabase.co"',
-          'import.meta.env.VITE_SUPABASE_ANON_KEY': '"sb_publishable_1EpIlW3NxMKtGL4MjF2xtg_aYacqCx3"',
+          'import.meta.env.VITE_SUPABASE_URL': '""',
+          'import.meta.env.VITE_SUPABASE_ANON_KEY': '""',
         }
-      : {},
+      : {
+          'import.meta.env.VITE_SUPABASE_URL':
+            '"https://ecflcrigkfyhifekwfxq.supabase.co"',
+          'import.meta.env.VITE_SUPABASE_ANON_KEY':
+            '"sb_publishable_1EpIlW3NxMKtGL4MjF2xtg_aYacqCx3"',
+        },
 }))
